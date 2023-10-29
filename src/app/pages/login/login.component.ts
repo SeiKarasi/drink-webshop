fd65b03348b02bc5cb2812d0afea63d43f8a4ff1
Pál Krisztián Zoltán <h050599@stud.u-szeged.hu>
2023-10-29 21:10:17 +0100

    Login page looks better

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -33,57 +33,13 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   login() {
     this.loginLoading = true;
-    // then akkor fut le, amikor a return ténylegesen lefut az adott Promiseban
-    // catch akkor fut le, ha hiba van benne
-    // finally mindig lefut
-    // FONTOS: Amit ezen a kódrészleten belül végzünk az később fut le,
-    // mint az esetleges későbbi sorok a függvényben 
-    /* this.loadingService.loadingWithPromise(this.LoginForm.get('email')?.value, this.LoginForm.get('password')?.value).then((_: boolean) => {
-      this.router.navigateByUrl('/main');
-    }).catch(error => {
-      console.error(error, 'Nem megfelelő bejelentkezési adatok!');
-    }).finally(() => {
-      console.log('This is executed finally.');
-    }); */
-
-    // async-await
-    // async arra utal, hogy aszinkron a függvény és await szerepel
-    // az await arra utal, hogy bevárunk egy Promiset
-    // párhuzamos futást tesz lehetővé az aszinkron
-    // az async-await technikával szinkron kódunk lesz, de aszinkron működés
-    // sima try-catch alkalmazása
-   /* try {
-      // then
-      const _ = await this.loadingService.loadingWithPromise(this.LoginForm.get('email')?.value, this.LoginForm.get('password')?.value);
-      this.router.navigateByUrl('/main');
-    } catch (error) {
-      // catch
-      console.error(error, 'Nem megfelelő bejelentkezési adatok!');
-    }
-    // finally
-    console.log('this is executed finally'); */
-
-    // Observable
-    // Ha nem zárjuk be akkor memory leak
-    // ezért le kell iratkozni ha már nem használjuk
-   /* this.loadingObservation = this.loadingService.loadingWithObservable(this.usersForm.get('email')?.value, this.usersForm.get('password')?.value);
-    this.loadingSubscription = this.loadingObservation.subscribe({
-      next: (data: boolean) => {
-      this.router.navigateByUrl('/main');
-      }, error: (error) => {
-        this.loginLoading = false;
-        console.error(error);
-      }, complete: () => {
-        this.loginLoading = false;
-        console.log('finally');
-      }
-    }); */
     this.authService.login(this.usersForm.get('email')?.value, this.usersForm.get('password')?.value)
     .then(credential => {
       console.log(credential);
       this.router.navigateByUrl('/main');
       this.toastr.success("Sikeres bejelentkezés!", "Bejelentkezés");
       this.loginLoading = false;
+      location.reload();
     }).catch(error => {
       console.error(error);
       this.toastr.error("Sikertelen bejelentkezés!", "Bejelentkezés");
